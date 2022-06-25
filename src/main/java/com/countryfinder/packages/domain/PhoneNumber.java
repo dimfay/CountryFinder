ce7319deb9d63f54758c46ffe083720def145256
Dmitrii Faierberg <dimfay27@gmail.com>
2022-06-25 21:10:30 +0300

    Hope last changes

--- src/main/java/com/countryfinder/packages/domain/PhoneNumber.java
+++ src/main/java/com/countryfinder/packages/domain/PhoneNumber.java
@@ -2,6 +2,7 @@ package com.countryfinder.packages.domain;
 
 import com.countryfinder.packages.validation.CoreError;
 import lombok.Data;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -9,8 +10,10 @@ import java.util.Collection;
 @Component
 @Data
 public class PhoneNumber {
+    @Nullable
     private Collection<CoreError> errors;
     private String number;
+    @Nullable
     private String country;
 
     public PhoneNumber(String number){
